ee70e99d80030ce95d7f2e516d148fc18c2ea885
shaakir <Alinoor11Alinoor11@gmail.com>
2026-04-17 06:44:14 +0100

    Upload profile done

--- forntend/src/Pages/Profile.jsx
+++ forntend/src/Pages/Profile.jsx
@@ -1,7 +1,33 @@
 import { useAppContext } from "../Context/AppContext";
+import { useRef, useState } from "react";
 
 const Profile = () => {
+  const fileRef = useRef(null);
   const { currentUser } = useAppContext();
+  const [imageURL, setImageURL] = useState(null);
+
+  // function to handle when image is uploaded
+  const handlefileChange = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "Card List");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dtx2xinlj/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+
+    const uploadedImageURL = await res.json();
+    setImageURL(uploadedImageURL.secure_url);
+  };
+
+  console.log(imageURL);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -10,8 +36,16 @@ const Profile = () => {
       </h1>
 
       <form className="flex flex-col gap-4">
+        <input
+          onChange={handlefileChange}
+          type="file"
+          ref={fileRef}
+          hidden
+        />
+
         <img
-          src={currentUser?.avatar}
+          onClick={() => fileRef.current.click()}
+          src={imageURL || currentUser?.avatar}
           alt="profile image"
           className="h-24 w-24 rounded-full cursor-pointer object-cover self-center mt-2"
         />
@@ -43,10 +77,14 @@ const Profile = () => {
           Update
         </button>
       </form>
+
       <div className="flex justify-between mt-5">
-        
-        <span className="text-red-700 curser-pointer">Delete account</span>
-        <span className="text-red-700 curser-pointer">Sign-in Out</span>
+        <span className="text-red-700 cursor-pointer">
+          Delete account
+        </span>
+        <span className="text-red-700 cursor-pointer">
+          Sign Out
+        </span>
       </div>
     </div>
   );
