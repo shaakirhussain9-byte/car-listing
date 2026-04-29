2eb7acce25ec4b7d792ec5154b3fb9f966d14c26
shaakir <Alinoor11Alinoor11@gmail.com>
2026-04-29 07:51:34 +0100

    search faction

--- forntend/src/Components/Hearder.jsx
+++ forntend/src/Components/Hearder.jsx
@@ -1,9 +1,26 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
-
+import { useState, useEffect } from "react";
 const Header = () => {
+  const navigate = useNavigate();
   const { currentUser } = useAppContext();
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search/?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-slate-200 shadow-md">
@@ -14,13 +31,20 @@ const Header = () => {
             <span className="text-slate-700">Auto</span>
           </h1>
         </Link>
-        <form className="flex items-center bg-slate-100 rounded-lg p-3">
+        <form
+          onSubmit={handleSearch}
+          className="flex items-center bg-slate-100 rounded-lg p-3"
+        >
           <input
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link
