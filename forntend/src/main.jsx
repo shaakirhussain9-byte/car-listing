4cacd140501b255e969845e65befeacaed30d77f
shaakirhussain9-byte <shaakirhussain9@gmail.com>
2026-05-05 16:51:59 +0100

    Fix import path for AppProvider in main.jsx

--- forntend/src/main.jsx
+++ forntend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { AppProvider } from './context/AppContext.jsx';
+import { AppProvider } from './Context/AppContext.jsx';
 
 
 const root = createRoot(document.getElementById('root'));
